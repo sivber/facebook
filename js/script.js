7353e23e27c39f7428e6ce1e4136a7ad3233dd90
Szymon <szymusk0212@gmail.com>
2024-02-05 00:16:31 +0100

    added create account form

--- js/script.js
+++ js/script.js
@@ -10,24 +10,53 @@ const loginBtn = document.querySelector(
 	'.main_input_container__form_button--login'
 );
 
+const createAccountContainer = document.querySelector(
+	'.create_account_container'
+);
+const createAccountButton = document.querySelector(
+	'.main_input_container__form_button--create_account'
+);
+
+const createAccount = document.querySelector('.create_account');
+
+const createAccountEmailInput = document.querySelector(
+	'.create_account_email_input'
+);
+
+const createAccountConfirmEmailInput = document.querySelector(
+	'.create_account_email_input_confirm'
+);
+
+const closeFormContainerButton = document.querySelector(
+	'.create_account_container__close_window'
+);
+
+const customSettingsInput = document.querySelector(
+	'.create_account_container__form--custom_settings'
+);
+
+const customSettingsContainer = document.querySelector(
+	'.create_account_container__form--custom_settings_container'
+);
+
 let hidenPassword = true;
 
 const showIcon = () => {
 	if (passwordInput.value.length === 0) {
-		if (!showPassword.classList.contains('hide-icon')) {
-			showPassword.classList.add('hide-icon');
+		if (!showPassword.classList.contains('hide-item')) {
+			showPassword.classList.add('hide-item');
 			hidenPassword = false;
 		}
-		if (!hidePassword.classList.contains('hide-icon')) {
-			hidePassword.classList.add('hide-icon');
+		if (!hidePassword.classList.contains('hide-item')) {
+			hidePassword.classList.add('hide-item');
 			hidenPassword = true;
 		}
 	} else if (passwordInput.value.length === 1) {
 		if (!hidenPassword) {
-			showPassword.classList.remove('hide-icon');
+			showPassword.classList.remove('hide-item');
 			hidenPassword = false;
 		} else {
-			hidePassword.classList.remove('hide-icon');
+			hidePassword.classList.remove('hide-item');
 			hidenPassword = true;
 		}
 	}
@@ -44,16 +73,48 @@ const changeInputType = () => {
 };
 
 const changeIcon = () => {
-	hidePassword.classList.toggle('hide-icon');
-	showPassword.classList.toggle('hide-icon');
+	hidePassword.classList.toggle('hide-item');
+	showPassword.classList.toggle('hide-item');
 };
 
 const sendToMainPage = () => {
-    if(emailInput.value === 'szymusk0212@gmail.com' && passwordInput.value === 'Szymon2005') {
-	window.location.href = '../facebook.html';
-    } else {
-        //popup
-    }
+	if (
+		emailInput.value === 'szymusk0212@gmail.com' &&
+		passwordInput.value === 'Szymon2005'
+	) {
+		window.location.href = '../facebook.html';
+	} else {
+		//popup
+	}
+};
+
+const showFormToCreateAccount = () => {
+	createAccountContainer.classList.toggle('hide-item');
+	createAccount.classList.toggle('darkened_background');
+};
+
+const hideFormToCreateAccount = () => {
+	createAccountContainer.classList.toggle('hide-item');
+	createAccount.classList.toggle('darkened_background');
+};
+
+function checkEmail() {
+	var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (regex.test(createAccountEmailInput.value)) {
+		if (createAccountConfirmEmailInput.classList.contains('hide-item')) {
+			createAccountConfirmEmailInput.classList.remove('hide-item');
+		}
+	} else {
+		createAccountConfirmEmailInput.classList.add('hide-item');
+	}
+}
+
+const showGenderCustomSettings = () => {
+	if (customSettingsInput.checked) {
+		customSettingsContainer.classList.toggle('hide-item');
+	} else {
+		customSettingsContainer.classList.toggle('hide-item');
+	}
 };
 
 hidePassword.addEventListener('click', changeIcon);
@@ -62,3 +123,7 @@ showPassword.addEventListener('click', changeInputType);
 hidePassword.addEventListener('click', changeInputType);
 passwordInput.addEventListener('input', showIcon);
 loginBtn.addEventListener('click', sendToMainPage);
+createAccountButton.addEventListener('click', showFormToCreateAccount);
+createAccountEmailInput.addEventListener('input', checkEmail);
+closeFormContainerButton.addEventListener('click', hideFormToCreateAccount);
+customSettingsInput.addEventListener('change', showGenderCustomSettings);
